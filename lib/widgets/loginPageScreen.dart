cb77f4f67079b103459bb3614c2276e7ff79a6cd
Salvatore D'Angelo <salvatore.dangelo@unicampania.it>
2021-06-21 11:28:02 +0200

    update librerie e errore nome file asset nell'import

--- lib/widgets/loginPageScreen.dart
+++ lib/widgets/loginPageScreen.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:formulario/Assets.dart';
+import 'package:formulario/assets.dart';
 import 'package:formulario/auth.dart';
 import 'package:formulario/googleSignInProvider.dart';
 import 'package:provider/provider.dart';
